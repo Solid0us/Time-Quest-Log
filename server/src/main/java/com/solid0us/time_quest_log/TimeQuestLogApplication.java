7f20c107c8507e7acca9802fcc908bc838296384
solid0us <blt11797@gmail.com>
2025-02-09 23:00:32 -0800

    Testing New Dockerfile Config

--- server/src/main/java/com/solid0us/time_quest_log/TimeQuestLogApplication.java
+++ server/src/main/java/com/solid0us/time_quest_log/TimeQuestLogApplication.java
@@ -1,9 +1,14 @@
 package com.solid0us.time_quest_log;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.solid0us.time_quest_log"})
 public class TimeQuestLogApplication {
 
 	public static void main(String[] args) {
